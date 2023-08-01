99033b617d9d59b9e231a9ff72313fa9ff6e229f
AvSidwll <sidwellavuyilebatyi@gmail.con>
2023-08-01 16:13:35 +0200

    last

--- app.js
+++ app.js
@@ -3,25 +3,25 @@ const fs = require('fs');
 const content1 = 'This is the first content.\n';
 fs.writeFile('data.txt', content1, (err) => {
   if (err) throw err;
-  console.log('Content 1 was written to data.txt');
+  console.log('Content 1 was written');
 });
 
 const content2 = 'This is the second content.\n';
 fs.writeFile('data.txt', content2, { flag: 'a' }, (err) => {
   if (err) throw err;
-  console.log('Content 2 was appended to data.txt');
+  console.log('Content 2 was appended');
 });
 
 const content3 = 'This is the third content.\n';
 fs.appendFile('data.txt', content3, (err) => {
   if (err) throw err;
-  console.log('Content 3 was appended to data.txt');
+  console.log('Content 3 was appended');
 });
 
-function Person(firstName, lastName, emailAddress) {
+function Person(firstName, lastName, email) {
     this.firstName = firstName;
     this.lastName = lastName;
-    this.emailAddress = emailAddress;
+    this.email = email;
   }
   
   module.exports = Person;
--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 
-function Person(firstName, lastName, emailAddress) {
+function Person(firstName, lastName, email) {
     this.firstName = firstName;
     this.lastName = lastName;
-    this.emailAddress = emailAddress;
+    this.email = email;
   }
   
   module.exports = Person;
   
-  // index.js
+ 
 
   const person1 = new Person('Sid', 'Bat', 'sid@gmail.com');
+  const person2 = new Person('Kai', 'Son', 'kaison@gmail.com');
+  const person3 = new Person('Mat', 'Turner', 'turner@gmail.com');
 
   console.log('Person 1:', person1);
+  console.log('Person 2:', person3);
+  console.log('Person 3:', person2);
